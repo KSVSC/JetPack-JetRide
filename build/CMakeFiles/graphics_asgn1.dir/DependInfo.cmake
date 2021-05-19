8e554c995b5958f711f51fa233b68bb52586e7d9
KSVSC <samhitaexplorica9@gmail.com>
2021-05-20 03:04:04 +0530

    added build files

---
+++ build/CMakeFiles/graphics_asgn1.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/ball.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/ball.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/bomerang.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/bomerang.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/bullet.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/bullet.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/circularring.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/circularring.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/color.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/color.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/display.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/display.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/firebeam.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/firebeam.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/fireline.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/fireline.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/fly_obj.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/fly_obj.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/fly_obj2.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/fly_obj2.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/fly_obj3.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/fly_obj3.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/ground.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/ground.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/input.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/input.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/magnet.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/magnet.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/main.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/main.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/nonedit.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/nonedit.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/number.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/number.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/other_handlers.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/other_handlers.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/player.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/player.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/stroke.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/stroke.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/timer.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/timer.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/viserion.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/viserion.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/wall.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/wall.cpp.o"
+  "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/src/waterbubble.cpp" "/home/ksvsc/IIITH/spring19/Graphics/assgn1/graphics-boilerplate/build/CMakeFiles/graphics_asgn1.dir/src/waterbubble.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libdrm"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
